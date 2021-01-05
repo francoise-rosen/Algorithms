991ed72144964138ef0c2c3284edd609aebd4b4a
four-of-seven <stan.barvinsky@gmail.com>
2021-01-05 23:47:04 +0100

    added an input for int, terminator word does not work so far

--- digitalRoot/digitalRoot/main.cpp
+++ digitalRoot/digitalRoot/main.cpp
@@ -55,12 +55,101 @@ void charToInt ()
     std::cout << sum << '\n';
 }
 
+void skipToInt()
+{
+    int count {0};
+    if (std::cin.fail())
+    {
+        std::cout << "clearing the failed state of istream\n";
+        std::cin.clear();
+        for (char ch; std::cin.get(ch);)
+        {
+            if (isdigit(ch))
+            {
+                std::cin.unget();
+                return;
+            }
+            else
+            {
+                std::cout << ch << '\n';
+                std::cout << "Still bad input, try to enter an int\n";
+                ++count;
+            }
+            if (count > 3)
+                throw std::runtime_error ("you have screwed the input! Terminating the programme!");
+        }
+    }
+}
+
+int getInt()
+{
+    int num;
+    while (true)
+    {
+        if (std::cin >> num)
+            return num;
+        else
+        {
+            std::cout << "Bad input, please enter an int!\n";
+            skipToInt();
+        }
+    }
+}
+
+int getIntInRange (int min, int max)
+{
+    while (true)
+    {
+        int temp = getInt();
+        if (temp >= min && temp <= max)
+            return temp;
+    }
+        
+}
+
+void runNumTimes (const int& numRolls, const int& min, const int& max)
+{
+    int count {0};
+    std::string term = "stop";
+    while ((count < numRolls))
+    {
+        std::string word;
+        std::cout << "Please enter a number between " << min << " and " << max
+        << " or terminate the program with word stop\n";
+        for (char ch; std::cin.get(ch);)
+        {
+            if (! isalpha (ch))
+            {
+                
+                std::cin.unget();
+                int temp = getIntInRange (min, max);
+                std::cout << digitalRoot (temp) << '\n';
+                ++count;
+                break;
+            }
+            else
+            {
+                std::cout << ch << '\n';
+                std::string temp;
+                std::cin.unget();
+                std::cin >> temp;
+                if (word == temp)
+                    return;
+            }
+            
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
     try
     {
         //std::cout << fak (5) << '\n';
-        int value = 12397;
-        std::cout << digitalRoot(value) << '\n';
+        //int value = 12397;
+        int value;
+        int min = 0;
+        int max = 100000;
+        runNumTimes(5, min, max);
     }
     catch (std::exception& e)
     {
