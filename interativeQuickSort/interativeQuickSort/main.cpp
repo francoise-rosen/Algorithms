d33a73a1fbb55f8c3339de19f47df0d01cde0435
four-of-seven <stan.barvinsky@gmail.com>
2021-01-13 11:15:01 +0100

    initial commit

---
+++ interativeQuickSort/interativeQuickSort/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  interativeQuickSort
+//
+//  Created by syfo_dias on 12/01/2021.
+//  Copyright © 2021 syfo_dias. All rights reserved.
+//
+
+#include <iostream>
+#include "quickSort.h"
+#include "bubbleSort.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int testArray[] {-122, -234, 0, 2, 3, -2, 187};
+    int size = sizeof (testArray) / sizeof (testArray[0]);
+    bubbleSort(testArray, testArray + size);
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << testArray[i] << " ";
+    }
+    std::cout << '\n';
+    return 0;
+}
---
+++ interativeQuickSort/interativeQuickSort/quickSort.h
@@ -0,0 +1,18 @@
+//
+//  quickSort.h
+//  interativeQuickSort
+//
+//  Created by syfo_dias on 12/01/2021.
+//  Copyright © 2021 syfo_dias. All rights reserved.
+//
+
+#ifndef quickSort_h
+#define quickSort_h
+
+template <typename ForwardIterator>
+void quickSort (ForwardIterator first, ForwardIterator last)
+{
+    
+}
+
+#endif /* quickSort_h */
