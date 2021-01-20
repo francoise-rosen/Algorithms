b6fbf6762ad772f1a9f80b50cc54d0a517e8b20c
four-of-seven <stan.barvinsky@gmail.com>
2021-01-20 10:23:53 +0100

    added instertion and selection sorts for forwarditerator

---
+++ interativeQuickSort/interativeQuickSort/insertionSort.h
@@ -0,0 +1,13 @@
+//
+//  insertionSort.h
+//  interativeQuickSort
+//
+//  Created by syfo_dias on 18/01/2021.
+//  Copyright © 2021 syfo_dias. All rights reserved.
+//
+
+#ifndef insertionSort_h
+#define insertionSort_h
+
+
+#endif /* insertionSort_h */
---
+++ interativeQuickSort/interativeQuickSort/selectionSort.h
@@ -0,0 +1,13 @@
+//
+//  selectionSort.h
+//  interativeQuickSort
+//
+//  Created by syfo_dias on 18/01/2021.
+//  Copyright © 2021 syfo_dias. All rights reserved.
+//
+
+#ifndef selectionSort_h
+#define selectionSort_h
+
+
+#endif /* selectionSort_h */
