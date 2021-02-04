9ce3ca1196968ea2ce1921a7ec93acc91b9672db
four-of-seven <stan.barvinsky@gmail.com>
2021-02-04 08:07:57 +0100

    first commit

---
+++ deterministicSelection/deterministicSelection/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  deterministicSelection
+//
+//  Created by syfo_dias on 04/02/2021.
+//  Copyright © 2021 syfo_dias. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+namespace algo
+{
+    template <typename ForwardIterator>
+    void choosePivot (ForwardIterator first, ForwardIterator last)
+    {
+        typename std::iterator_traits<ForwardIterator>::difference_type size;
+        size = std::distance (first, last);
+        
+    }
+    
+    /** recursively find the median of C. */
+    template <typename ForwardIterator>
+    void findMedian (ForwardIterator first, ForwardIterator last)
+    {
+        
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
