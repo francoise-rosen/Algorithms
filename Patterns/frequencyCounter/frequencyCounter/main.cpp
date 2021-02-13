8fdbc8dadb0da47d3c703250d9e8f4c29ceb2db3
four-of-seven <stan.barvinsky@gmail.com>
2021-02-13 10:03:44 +0100

    initial commit

---
+++ Patterns/frequencyCounter/frequencyCounter/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  frequencyCounter
+//
+//  Created by syfo_dias on 13/02/2021.
+//  Copyright © 2021 syfo_dias. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+/**
+   Find if elements of second array are squared elements of first
+   Frequency must match
+ */
+
+bool frequencyCounter (const std::vector<int>& arr1, const std::vector<int>& arr2)
+{
+    if (arr1.size() != arr2.size())
+        return false;
+    bool match = false;
+    return match;
+}
+
+
+int main(int argc, const char * argv[]) {
+    try {
+        
+    } catch (std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
+    return 0;
+}
