bd4d47296a0a1738c204a2a727eeb4d7be966dac
four-of-seven <stan.barvinsky@gmail.com>
2021-02-14 13:16:27 +0100

    added iterative On solution

--- Patterns/SlidingWindow/SlidingWindow/main.cpp
+++ Patterns/SlidingWindow/SlidingWindow/main.cpp
@@ -30,10 +30,49 @@ T maxSubArraySumNaive (const C& array, int num = 3)
     
 }
 
+template <typename C, typename T = typename C::value_type>
+T maxSubArraySumSlidingWindow (const C& array, int n = 3)
+{
+    if (array.size() < n || n < 1)
+        return static_cast<T>(0);
+    /** sum first n values. save the temp sum
+        move pointer to i + n, subtract array[i], add array[i + n] and replace temp sum
+     */
+    using iterator = typename C::iterator;
+    using const_iterator = typename C::const_iterator;
+    const_iterator c_first = array.begin();
+    const_iterator sub_last = c_first;
+    std::advance (sub_last, n);
+    const_iterator c_last = array.end();
+    
+    T maxSum = 0;
+    T tempSum = 0;
+    
+    for (auto it = c_first; it != sub_last; ++it) // O(n)
+        maxSum += *it;
+
+    tempSum = maxSum;
+    while (sub_last != c_last) // O(size)
+    {
+        tempSum = tempSum - *c_first + *sub_last;
+        ++c_first; ++sub_last;
+        if (tempSum > maxSum) maxSum = tempSum;
+    }
+    return maxSum;
+}
+
 void testNaive()
 {
     std::vector<int> v {1, 2, 3, 4, 5 ,6, 0};
     int res = maxSubArraySumNaive(v);
+    std::cout << res << '\n';
+    assert (res == 15);
+}
+
+void testSliding()
+{
+    std::vector<int> v {1, -2, 3, 4, 5 ,6, 0};
+    int res = maxSubArraySumSlidingWindow (v);
     std::cout << res << '\n';
     assert (res == 15);
 }
@@ -43,6 +82,7 @@ int main(int argc, const char * argv[]) {
     try {
         std::cout << std::numeric_limits<double>::min() << '\n' << std::numeric_limits<double>::lowest() << '\n';
         testNaive();
+        testSliding();
     } catch (std::exception& e) {
         std::cerr << e.what() << '\n';
         return 1;
