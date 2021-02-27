489f85528efee161df2f3455df66779a0abb841a
four-of-seven <stan.barvinsky@gmail.com>
2021-02-27 19:06:05 +0100

    updated function

--- twoSum/twoSum/main.cpp
+++ twoSum/twoSum/main.cpp
@@ -9,22 +9,22 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <set>
 
-using Pair = std::vector<std::pair<int, int>>;
+using vPair = std::set<std::pair<int, int>>;
 
-Pair twoSum (const std::vector<int>& array, int s)
+vPair twoSum (const std::vector<int>& array, int s)
 {
-    Pair result;
-    std::unordered_map<int, int> hashTable;
-    for (int i = 0; i < array.size(); ++i)
-    {
-        hashTable[array[i]];
-    }
+    vPair result;
+    std::unordered_map<int, bool> hashTable;
     
     for (auto elem : array)
     {
         if (hashTable.find(s - elem) != hashTable.end())
-            result.push_back (std::make_pair (elem, s - elem));
+        {
+            result.insert (std::make_pair (elem, s - elem));
+        }
+        hashTable[elem] = true;
     }
     return result;
 }
@@ -33,7 +33,7 @@ Pair twoSum (const std::vector<int>& array, int s)
 
 int main(int argc, const char * argv[]) {
     std::vector<int> array {3, 5, 2, -4, 8, 11};
-    Pair res = std::move (twoSum(array, 7));
+    vPair res = twoSum(array, 7);
     for (auto p : res)
     {
         std::cout << "[ " << p.first << " " << p.second << " ]\n";
