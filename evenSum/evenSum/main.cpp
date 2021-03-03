e9977726dce0389c2e3dd28b374c405814d37684
four-of-seven <stan.barvinsky@gmail.com>
2021-03-03 22:06:15 +0100

    add solution with reverse_iterator

--- evenSum/evenSum/main.cpp
+++ evenSum/evenSum/main.cpp
@@ -13,10 +13,8 @@
 // add up all the elems in container counting from back starting from 1
 // every even index (if counting backwards) must be multiplied by 2
 
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::vector<int> array {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int sumVersion1 (std::vector<int>& array)
+{
     typename std::vector<int>::iterator it;
     it = array.end();
     --it;
@@ -29,7 +27,28 @@ int main(int argc, const char * argv[]) {
     }
     // add the first element
     sum += (count % 2 == 0) ? *it * 2: *it;
-    std::cout << sum << '\n';
+    return sum;
+}
+
+int sumVersion2 (std::vector<int>& array)
+{
+    typename std::vector<int>::reverse_iterator it = array.rbegin();
+    int sum {0};
+    bool even {false};
+    while (it != array.rend())
+    {
+        sum += (even) ? *it * 2 : *it;
+        ++it;
+        even = (even == true) ? false : true;
+    }
+    return sum;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::vector<int> array {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::cout << sumVersion1 (array) << '\n';
+    std::cout << sumVersion2 (array) << '\n';
     
     return 0;
 }
